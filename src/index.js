af19efe45fcef58bca829031f8f360b4d12b6642
Devin Buntin <devin.buntin@gmail.com>
2021-05-03 14:33:34 -0500

    Main Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    // The constructor function is not required
+    // by React, BUT
+    // 'constructor(props)' and 'super(props)' must be
+    // called in this way in order to use State.
+    constructor(props) {
+        super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        // THIS IS THE ONLY TIME we do direct assignment
+        // to 'this.state'!
+        this.state = { lat: null, errorMessage: '' }
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                // You must always call 'setState' anytime
+                // you want the state to change/update!!
+                this.setState({ lat: position.coords.latitude });
+            },
+            err => {
+                this.setState({ errorMessage: err.message })
+            }
+        );
+    }
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+    }
+
+    // React says that we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>;
+    }
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
